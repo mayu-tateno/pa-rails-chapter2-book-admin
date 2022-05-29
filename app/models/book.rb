86871dd3c09378bdc1297b39fe6c44bda006d70b
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-05-29 14:50:31 +0900

    feat: scopeを定義 #2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  # scopeを定義するメリット
+  # 繰り返し利用するクエリの再利用性が上がる。
+  # クエリに名前をつけることで可読性が上がる。
+  scope :costly, -> { where("proce > ?", 3000) }
+
+  # 特定のパラメータを利用するscopeも定義可能。
+  scope :written_about, -> (theme) { where("name like ?", "%#{theme}%") }
+  # Book.costly.written_about("java") などのように重ねて呼び出すことも可能。可読性も高い。(普通のSQLを書く場合は一度に全部指定しないといけないので、こうはいかない！単体でもメソッドチェインでも呼び出せる！)
 end
