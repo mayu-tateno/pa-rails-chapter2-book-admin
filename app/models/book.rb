6c2778cd8c270777108910445187f3c2b049e015
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-06-04 22:01:08 +0900

    feat: validateブロックを使用したバリデーションを定義 #2

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,4 +15,12 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  # validateブロック内で独自のバリデーションを行う例。
+  # errorsに何かしらの値が入っていればバリデーション失敗と判定されるため、自分でバリデーションを定義する場合は不正になる条件ならばerrorsにエラーメッセージを付与するという方法でバリデーションを行こなう。
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
 end
